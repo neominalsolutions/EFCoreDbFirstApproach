28aef6836ebc34763de9448f2650997445a2854f
mert.alptekin <mert.alptekin@neominal.com>
2022-01-18 19:00:33 +0300

    Add project files.

---
+++ EFCoreDbFirstApproach/Data/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreDbFirstApproach.Data
+{
+    public partial class Address
+    {
+        public Guid Id { get; set; }
+        public string StreetName { get; set; }
+        public string Provience { get; set; }
+        public string City { get; set; }
+        public int PersonId { get; set; }
+
+        public virtual Person Person { get; set; }
+    }
+}
---
+++ EFCoreDbFirstApproach/Data/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreDbFirstApproach.Data
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public string Information { get; set; }
+
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
